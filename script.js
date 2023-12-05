9475fea45b09e0dd502f5064fa74a52978c93746
Awais <tommy221829@gmail.com>
2023-12-05 14:38:24 +0500

    Everything is commited

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("kill him from")
+const obj = {
+    log : ["example" , "Test"],
+    get latest(){
+        if(this.log.length===0)
+        return this.log[this.log.length - 1]
+    },
+}
+
+console.log(obj.latest)
+
+
+class Myclass{
+    constructor(){
+        this._myProperty =45
+    }
+    get myProperty(){
+        return this._myProperty
+    }
+}
+const myObjext =new Myclass()
+console.log(myObjext.myProperty)
